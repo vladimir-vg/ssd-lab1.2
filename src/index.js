af56202741b54ba1adcd039d7fc94c55e923ef18
Vladimir Gordeev <gordeev.vladimir.v@gmail.com>
2021-08-18 10:25:34 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 
 const reversedCharNumbers = (str) => {
   const baseCode = 'a'.charCodeAt(0)
-  const chars = str.split('');
+  const chars = str.toLowerCase().split('');
   chars.reverse();
-  const charToCharNumber = c => baseCode - c.charCodeAt(0) + 1;
+  const charToCharNumber = c => c.charCodeAt(0) - baseCode + 1;
   return chars.map(charToCharNumber).join(' ');
 };
 
